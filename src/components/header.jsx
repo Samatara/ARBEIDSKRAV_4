c3fa9712dfd1203ac84f1274cb6df688f3ff245f
arditrm <ardit.ramadani904@gmail.com>
2026-04-27 15:53:36 +0200

    lagre
    lagre

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+
+
+
+function header({gruppenummer}){ 
+    return (
+        <header className="side-header">
+            <p className="kurs-merkelapp">UIN - Utvikling av intraktive nettsteder</p>
+            <h1 className="gruppe-tittel">
+                gruppe <span className="gruppw-nummer">{gruppenummer}</span>
+            </h1>
+            <p className="undertittel"> arbeidskarv 4</p>
+        </header>
+    )
+}
+export default header 

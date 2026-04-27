0ee67ee3e62cee5c1a2682f93600e23e24725748
arditrm <ardit.ramadani904@gmail.com>
2026-04-27 15:02:38 +0200

    lagre
    profil

--- src/components/ProfilArdit.jsx
+++ src/components/ProfilArdit.jsx
@@ -3,6 +3,22 @@ function ProfilArdit() {
     const navn = "Ardit Ramadani"
     const epost = "arditr@hiof.no"
     const studie = "bachelor i informasjonsystemer"
-    canst bilde = ""
+    const bilde = ""
+  
+   return( 
 
+    <article className="profil-ardit">
+        <figure className="ardit-bilde-ramme">
+            <img src={bilde} alt={`portrett av ${navn}`} className="ardit-bilde"/>
+            
+        </figure>
+        <section className="ardit-info">
+            <h3 className="ardit-navn">{navn}</h3>
+            <p className="ardit-studie">{studie}</p>
+            <a href={`milto:${epost}`} className="ardit-epost">{epost}</a>
+        </section>
+    </article>
+   )
 }
+
+export default ProfilArdit

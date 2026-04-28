9cb800ab6a12d3fa6dabf8dae9c416d48aadcc21
Samatar <samatarbulbula@gmail.com>
2026-04-28 22:24:28 +0200

    css
    justerte litt på class navne dersom de ikke var help samme

--- src/components/ProfilSamatar.jsx
+++ src/components/ProfilSamatar.jsx
@@ -8,16 +8,15 @@ function ProfilSamatar(){
     const bilde= "/sama.png "
     return(
         <article className="profil-samatar">
-        <header className="samtar-topp"/>
         <img src={bilde} alt={`portrett av ${navn}`} className="samatar-bilde"/>
-        <section>
-        <h3 className="samatar-navn">{navn}</h3>
-        <dl className="detaljer">
+        <section className='samatar-info '>
+        <h3 className="samatar-navn ">{navn}</h3>
+        <dl className="samatar-detaljer">
             <dt>Studie</dt>
             <dd>{studie}</dd>
             <dt>Kontakt</dt>
             <dd>
-                <a href={`mailto:${epost}`} className="epost">{epost}</a>
+                <a href={`mailto:${epost}`} className="samatar-epost">{epost}</a>
             </dd>
         </dl>
         </section>
